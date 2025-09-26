00b228fbffaf8c647def473ce5e9cd4908cd975a
Albert Kuular <albert.kuular@gmail.com>
2025-09-26 13:30:05 +0700

    Added api route

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
 }
